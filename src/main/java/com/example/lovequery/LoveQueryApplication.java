335237c96e2720b02fbab9f289005e731aa72da0
eunse <euntori@eunseui-MacBookAir.local>
2025-11-11 01:36:46 +0900

    initial commit

---
+++ src/main/java/com/example/lovequery/LoveQueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.lovequery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveQueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lovequery/LoveQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lovequery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
